082680a5a2462516c687668b1fac69573f360ef3
romzyyy <fijay0117@gmail.com>
2022-12-11 15:28:00 +0700

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState('welcome to my app');
+    const [age, setAge] = useState(20);
+
+    const changeTitle = () => {
+        setTitle('title Chaged');
+        setAge(50);
+    };
+    return (
+        <div>
+            <Header />
+            <h1>{title}</h1>
+            <h1>Age : {age}</h1>
+            <button onClick={changeTitle}>Change Title</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div>
+        <h2>INI ADALAH COMPONENTS HEADER</h2>
+    </div>
+  )
+}
+
+export default Header
